8d1cf4da135361993127e239cfc07379b30296b4
Bill McKessy <bill@evident.io>
2016-10-29 08:16:50 -0700

    yes

--- webapp/draw_memory.js
+++ webapp/draw_memory.js
@@ -27,10 +27,23 @@ function draw_memory () {
 
   // var scale_x = d3.scaleLinear().domain([0, mem_width]).range([])
 
+  var howthisworks = ''
+  howthisworks += '<h4>how this works</h4>'
+  howthisworks += ['Click [draw memory].  That renders the memory layout of the first 2048 bytes of the NES above. ',
+    'The values are the brightness of the rectangle, and a blue stroke indicates that memory address is active. ',
+    'Click the memory address to add a probe and a doper.  The probe will update whenever the value is read.  ',
+    'For some addresses this is only a handful of times per level, others are read 100 times per frame.  ',
+    'Click activate to change the value to whatever you have set at the slider.  ',
+    'The value of the probe should be locked to the value set with the slider, indicating the memory is being replaced by your custom value.',
+    'To record your own run...',
+    'You need to stop the run that is currently playing..',
+    'Press [stop] > [is loading inputs] > [reset nes] > [record] > play your run > [stop] > [write to disk] > [restart]'
+  ].join('<br><br>')
+
   var div_explain = div_memory.append('div')
     .style('max-height', '100px')
     .style('overflow-y', 'scroll')
-    .html('Click [draw memory].  That renders the memory layout of the first 2048 bytes of the NES above.  The values are the brightness of the rectangle, and a blue stroke indicates that memory address is active.  Click the memory address to add a probe and a doper.  The probe will update whenever the value is read.  For some addresses this is only a handful of times per level, others are read 100 times per frame.  Click activate to change the value to whatever you have set at the slider.  The value of the probe should be locked to the value set with the slider, indicating the memory is being replaced by your custom value.')
+    .html(howthisworks)
 
   var m = []
   for (var i = 0; i < n_memory_elements; i++) {
--- webapp/index.js
+++ webapp/index.js
@@ -72,7 +72,7 @@ require('./load_romfile.js')(function () {
 
   // memory address 9 = flashing speed of icons
 
-  // add_both({ name: 'what_block_loaded_0', addr: 1697, value: 12, active: false })
+  add_both({ name: 'custom block', addr: 1697, value: 12, active: false })
   // add_both({ name: 'what_block_loaded_1', addr: 1698, value: 12 })
   // add_both({ name: 'what_block_loaded_2', addr: 1699, value: 12 })
   // add_both({ name: 'what_block_loaded_3', addr: 1700, value: 12 })
